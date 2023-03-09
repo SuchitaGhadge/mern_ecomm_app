b6823c02c76fdfc5c1bcde351d1d425fba669975
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-03-09 11:41:16 +0530

    middleware sessions

--- backend/app.js
+++ backend/app.js
@@ -5,18 +5,28 @@
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+// console.log("process",process.env)
 
-mongoose.connect("mongodb://localhost:27017/ecomm", {
+
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 }).then(() => {
     console.log("DB CONNECTED")
 }).catch((err) => {
     console.log(`DB CONNECTION FAILED WITH ${err}`)
 });
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server is running at port ${port}`);
 });
+
+app.use(cors());
+app.use(cookieParser());
+app.use(bodyParser.json());
--- testback/index.js
+++ testback/index.js
@@ -2,6 +2,21 @@ const express = require('express');
 const app = express();
 
 const port = 8000;
+
+const admin = (req, res) => res.send("this is Admin Dashboard")
+
+// middleware
+const isAdmin = (req, res, next) => {
+    console.log("is admin is running");
+    next();
+}
+
+const isLoggedIn = (req, res, next) => {
+    console.log("is loggedIn is running");
+    next();
+}
+
+// end
 app.get("/", (req, res) => res.send("Home page"));
 
 app.get("/login", (req, res) => res.send("you are visiting login route"));
@@ -9,5 +24,7 @@ app.get("/login", (req, res) => res.send("you are visiting login route"));
 app.get('/logout', (req, res) => res.send("You are visiting logout route"));
 
 app.get("/signup", (req, res) => res.send('you are visiting signup route'));
+
+app.get("/admin", isLoggedIn, isAdmin, admin);
 
 app.listen(port, () => console.log(`Server is up and running at port ${port}`));

616b4d1c75daffdfd9ee152785178547780232ef
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-03-11 21:51:10 +0530

    validation for signup route

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,6 +1,16 @@
-const User = require("../models/user")
+const User = require("../models/user");
+const { validationResult } = require('express-validator');
 exports.signup = (req, res) => {
-    console.log("REQ BODY", req.body);
+    // console.log("REQ BODY", req.body);
+
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(422).json({errors : errors.array()}) 
+        /*
+         * 422 Unprocessable Content
+         * The HyperText Transfer Protocol (HTTP) 422 Unprocessable Content response status code indicates that the server understands the content type of the request entity, and the syntax of the request entity is correct, but it was unable to process the contained instructions.
+         */
+    }
     const user = new User(req.body);
 
     user.save((err, user) => {
@@ -23,3 +33,16 @@ exports.signout  = (req, res) => {
         message : 'user signout success'
     })
 }
+
+/*
+ * your server will respond like this: 
+{
+  "errors": [
+    {
+      "location": "body",
+      "msg": "Invalid value",
+      "param": "username"
+    }
+  ]
+}
+ */
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,8 +1,13 @@
- var express = require('express');
+var express = require('express');
 const { signout, signup } = require('../controllers/authentication');
- var router = express.Router();
+const { body } = require('express-validator');
+var router = express.Router();
 
- router.post("/signup", signup)
+ router.post("/signup",[
+    body('name', "name should be at least 2 char").isLength({min: 2}),
+    body('email', "email is required").isEmail(),
+    body('password', "password should be at least 3 char").isLength({min: 3})
+ ], signup)
  router.get("/signout", signout)
 
  module.exports = router;

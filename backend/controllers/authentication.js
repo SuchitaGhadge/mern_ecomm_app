6336462c6a84b32ed87a571fa69c52d0c31cb512
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-03-12 17:10:49 +0530

    authorization

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,41 +1,105 @@
 const User = require("../models/user");
-const { validationResult } = require('express-validator');
+const { validationResult } = require("express-validator");
+const jwt = require("jsonwebtoken");
+const { expressjwt } = require('express-jwt');  //newer version format
+
 exports.signup = (req, res) => {
-    // console.log("REQ BODY", req.body);
+  // console.log("REQ BODY", req.body);
 
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(422).json({errors : errors.array()}) 
-        /*
-         * 422 Unprocessable Content
-         * The HyperText Transfer Protocol (HTTP) 422 Unprocessable Content response status code indicates that the server understands the content type of the request entity, and the syntax of the request entity is correct, but it was unable to process the contained instructions.
-         */
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+    /*
+     * 422 Unprocessable Content
+     * The HyperText Transfer Protocol (HTTP) 422 Unprocessable Content response status code indicates that the server understands the content type of the request entity, and the syntax of the request entity is correct, but it was unable to process the contained instructions.
+     */
+  }
+  const user = new User(req.body);
+
+  user.save((err, user) => {
+    if (err) {
+      return res.status(400).json({
+        err: "NOT able to save user in DB",
+      });
     }
-    const user = new User(req.body);
+    res.json({
+      name: user.name,
+      email: user.email,
+      id: user._id,
+    });
+  });
+};
 
-    user.save((err, user) => {
-        if(err){
-            return res.status(400).json({
-                err : "NOT able to save user in DB"
-            });
-        }
-        res.json({
-            name: user.name,
-            email: user.email,
-            id: user._id
-        })
+exports.signin = (req, res) => {
+  const { email, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+
+  User.findOne({ email }, (err, user) => {
+    if (err || !user ) {
+      return res.status(400).json({
+        error: "USER email does not exists",
+      });
+    }
+
+    if(!user.authenticate(password)){
+      return res.status(401).json({
+        error : "Email and Password do not match"
+      })  
+    }
+
+    // create token
+    const token = jwt.sign({_id: user._id}, process.env.SECRET_KEY);
+
+    // put token in cookie
+    res.cookie("token", token, { expiresIn: '1h' });
+
+    // send response to frontend
+    const { _id, name, email, role } = user;
+    return res.json({ token, user: { _id, name, email, role }})
+    
+  });
+};
+
+exports.signout = (req, res) => {
+  res.clearCookie("token");
+  res.json({
+    message: "user signout success"
+  });
+};
+
+// protected route
+exports.isSignedIn = expressjwt({
+  secret : process.env.SECRET_KEY,
+  userProperty : "auth",
+  algorithms: ["HS256"]   //required in newer version of express-jwt
+});
+
+//  custom middleware
+exports.isAuthenticated = (req, res, next) => {
+  let checker = req.profile && req.auth && req.profile._id === req.auth._id
+
+  if(!checker){
+    return res.status(403).json({
+      error : "ACCESS DENIED"
     })
-  
+  }
+  next();
 }
 
-exports.signout  = (req, res) => {
-    res.json({
-        message : 'user signout success'
+exports.isAdmin = (req, res,next) => {
+  if(req.profile.role !== 1){
+    return res.status(403).json({
+      error : "You are not ADMIN, Access Denied"
     })
+  }
+  next();
 }
 
 /*
- * your server will respond like this: 
+ * your server will respond like this in signup: 
 {
   "errors": [
     {
--- backend/routes/authentication.js
+++ backend/routes/authentication.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { signout, signup } = require('../controllers/authentication');
+const { signout, signup, signin, isSignedIn } = require('../controllers/authentication');
 const { body } = require('express-validator');
 var router = express.Router();
 
@@ -7,7 +7,20 @@ var router = express.Router();
     body('name', "name should be at least 2 char").isLength({min: 2}),
     body('email', "email is required").isEmail(),
     body('password', "password should be at least 3 char").isLength({min: 3})
- ], signup)
- router.get("/signout", signout)
+ ], signup);
+
+ router.post("/signin",[
+    body('email', "email is required").isEmail(),
+    body('password', "password should be at least 3 char").isLength({min: 1})
+ ], signin);
+
+ router.get("/signout", signout);
+
+//  protected route
+ router.get("/testroute", isSignedIn, (req, res) => {
+   res.json(req.auth)
+ })
+
+
 
  module.exports = router;

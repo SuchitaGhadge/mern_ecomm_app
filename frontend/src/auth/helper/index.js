769c9f1d860510b4e7320840ed7c1a35eae16794
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-04-30 00:33:45 +0530

    helper methods

--- frontend/src/auth/helper/index.js
+++ frontend/src/auth/helper/index.js
@@ -5,7 +5,7 @@ export const signup = user => {
     return fetch(`${API}signup`, {
         method: "POST",
         headers: {
-            "ACCEPT": "application/json",
+            ACCEPT: "application/json",
             "Content-Type": "application/json"
         },
         body: JSON.stringify(user)
@@ -13,3 +13,43 @@ export const signup = user => {
     .then(response => response.json())
     .catch(err => console.error("signup err", err))
 }
+
+export const signin = user => {
+    return fetch(`${API}signin`, {
+        method: "POST",
+        headers: {
+            ACCEPT: "application/json",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .catch(err => console.error("signup err", err))
+}
+
+export const authenticate = (data, next) => {
+    if(typeof window !== "undefined"){
+        localStorage.setItem('jwt', JSON.stringify(data));
+        next();
+    }
+}
+
+export const signout = next => {
+    if(typeof window !== 'undefined'){
+        localStorage.removeItem("jwt");
+        next();
+
+        return fetch(`${API}signout`, {
+            method: "GET"
+        })
+        .then(response => console.log("Signout success"))
+        .catch(err => console.error("signout error", err))
+    }
+}
+
+export const isAuthenticated = () => {
+    if(typeof window == 'undefined') false;
+
+    if(localStorage.getItem("jwt")) JSON.parse(localStorage.getItem("jwt"))
+    else false
+} 

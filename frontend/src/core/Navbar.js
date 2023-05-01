d601299f3c26310a11f99487df03d2fa33f64271
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-05-02 01:59:36 +0530

    hide show logic of signin, signup and signout

--- frontend/src/core/Navbar.js
+++ frontend/src/core/Navbar.js
@@ -1,60 +1,72 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
+import React, {Fragment} from 'react'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { isAuthenticated, signout } from '../auth/helper';
 
 export default function Navbar() {
+  const navigate = useNavigate()
   return (
-        <header className='bg-dark'>
+        <header className='bg-dark mt-2'>
             <nav className='flex justify-center items-center w-[92%]'>
                 <ul className='flex items-center gap-[4vw]'>
-                    <li className='hover:text-gray-500'>
+                    <li>
                         <NavLink  style={({ isActive }) => {
                             return {
                               color: isActive ? "green" : "white"
                             };
                           }} to='/'>Home</NavLink>
                     </li>
-                    <li className='hover:text-gray-500'>
+                    <li>
                         <NavLink style={({ isActive }) => {
                             return {
                               color: isActive ? "green" : "white"
                             };
                           }}  to='/cart'>Cart</NavLink>
                     </li>
-                    <li className='hover:text-gray-500'>
+                    <li>
                         <NavLink style={({ isActive }) => {
                             return {
                               color: isActive ? "green" : "white"
                             };
                           }}  to='/user/dashboard'>Dashboard</NavLink>
                     </li>
-                    <li className='hover:text-gray-500'>
+                    <li>
                         <NavLink style={({ isActive }) => {
                             return {
                               color: isActive ? "green" : "white"
                             };
                           }}  to='/admin/dashboard'>Admin Dashboard</NavLink>
                     </li>
-                    <li className='hover:text-gray-500'>
-                        <NavLink style={({ isActive }) => {
-                            return {
-                              color: isActive ? "green" : "white"
-                            };
-                          }}  to='/signup'>Sign Up</NavLink>
+                    {!isAuthenticated() && (
+                      <Fragment>
+                      <li>
+                          <NavLink style={({ isActive }) => {
+                              return {
+                                color: isActive ? "green" : "white"
+                              };
+                            }}  to='/signup'>Sign Up</NavLink>
+                      </li>
+                      <li>
+                          <NavLink style={({ isActive }) => {
+                              return {
+                                color: isActive ? "green" : "white"
+                              };
+                            }}  to='/signin'>Sign In</NavLink>
+                      </li>
+                      </Fragment>
+                    )}
+                    {isAuthenticated() && (
+                      <li>
+                      <span className='text-yellow-500 hover:text-yellow-400 hover:font-semibold'
+                      onClick={() => {
+                        signout(() => {
+                          navigate('/')
+                        })
+                      }}
+                      >
+                          Signout
+                        </span>
                     </li>
-                    <li className='hover:text-gray-500'>
-                        <NavLink style={({ isActive }) => {
-                            return {
-                              color: isActive ? "green" : "white"
-                            };
-                          }}  to='/signin'>Sign In</NavLink>
-                    </li>
-                    <li className='hover:text-gray-500'>
-                        <NavLink style={({ isActive }) => {
-                            return {
-                              color: isActive ? "green" : "white"
-                            };
-                          }} to='/signout'>Sign Out</NavLink>
-                    </li>
+                    )}
                 </ul>
             </nav>
         </header>

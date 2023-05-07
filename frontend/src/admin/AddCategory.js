783a2829cfcf837d67ff8c7eafd2176f4aa2e097
SuchitaGhadge <suchita8494.sg@gmail.com>
2023-05-07 23:34:30 +0530

    create product form with a bug

--- frontend/src/admin/AddCategory.js
+++ frontend/src/admin/AddCategory.js
@@ -12,72 +12,95 @@ export default function AddCategory() {
   const { user, token } = isAuthenticated();
 
   const changeHandler = (event) => {
-    setError(false)
-    setName(event.target.value)
-  }
+    setError(false);
+    setName(event.target.value);
+  };
 
   const onClose = () => {
     setError(false);
-    setSuccess(false)
-  }
+    setSuccess(false);
+  };
 
   const errorMessage = () => {
     return (
-        <div className="flex items-center justify-center w-full">
-            <div className="w-1/2 px-6">
-            <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert"
-            style={{display: error ? "" : "none"}}
-    >
-        <span className="font-bold">Failed to create category</span>
-        <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
-        </span>
-    </div></div>
+      <div className="flex items-center justify-center w-full">
+        <div className="w-1/2 px-6">
+          <div
+            className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+            role="alert"
+            style={{ display: error ? "" : "none" }}
+          >
+            <span className="font-bold">Failed to create category</span>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg
+                className="fill-current h-6 w-6 text-red-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+              </svg>
+            </span>
+          </div>
         </div>
-    )
-  }
+      </div>
+    );
+  };
 
   const successMessage = () => {
     return (
-        <div className="flex items-center justify-center w-full">
-            <div className="w-1/2 px-6">
-            <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert"
-        style={{display: success ? "" : "none"}}
-    >
-        <span className="font-bold">Category created successsfully</span>
-        <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
-            <svg className="fill-current h-6 w-6 text-green-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
-            onClick={onClose}
-            /></svg>
-        </span>
-    </div></div>
+      <div className="flex items-center justify-center w-full">
+        <div className="w-1/2 px-6">
+          <div
+            className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+            role="alert"
+            style={{ display: success ? "" : "none" }}
+          >
+            <span className="font-bold">Category created successsfully</span>
+            <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+              <svg
+                className="fill-current h-6 w-6 text-green-500"
+                role="button"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 20 20"
+              >
+                <title>Close</title>
+                <path
+                  d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+                  onClick={onClose}
+                />
+              </svg>
+            </span>
+          </div>
         </div>
-    )
-  }
+      </div>
+    );
+  };
 
   const onSubmit = (event) => {
     event.preventDefault();
-    setError(false)
-    setSuccess(false)
+    setError(false);
+    setSuccess(false);
 
     // backend request
-    createCategory(user._id, token, {name})
-    .then(data => {
-      // console.log("category data ", data)
-      if(data.error){
-        setError(true)
-      }else{
-        setError(false)
-        setSuccess(true)
-        setName("")
-      }
-    })
-    .catch()
-  }
-  
+    createCategory(user._id, token, { name })
+      .then((data) => {
+        // console.log("category data ", data)
+        if (data.error) {
+          setError(true);
+        } else {
+          setError(false);
+          setSuccess(true);
+          setName("");
+        }
+      })
+      .catch();
+  };
+
   const myCategoryForm = () => (
     <form className="mx-auto">
-      <div className="w-9/12 mx-auto px-12 py-4" >
+      <div className="w-9/12 mx-auto px-12 py-4">
         <p className="pb-2 text-base text-semibold">Enter the Category</p>
         <input
           type="text"
@@ -88,19 +111,26 @@ export default function AddCategory() {
           onChange={changeHandler}
           value={name}
         />
-        <button className="my-2 px-4 py-2 border border-cyan-600 focus:outline-none focus:ring focus:border-none  text-cyan-600 rounded" onClick={onSubmit}>Create Category</button>
+        <button
+          className="my-2 px-4 py-2 border border-cyan-600 focus:outline-none focus:ring focus:border-none  text-cyan-600 rounded"
+          onClick={onSubmit}
+        >
+          Create Category
+        </button>
       </div>
     </form>
   );
 
   const goToDashboard = () => (
     <div className="px-2 pt-5">
-        <Link to='/admin/dashboard' className="bg-gray-600 text-sm px-4 py-2 text-white border border-transparent transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700 duration-300">
+      <Link
+        to="/admin/dashboard"
+        className="bg-gray-600 text-sm px-4 py-2 text-white border border-transparent transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700 duration-300"
+      >
         Admin Home
       </Link>
-       
     </div>
-  )
+  );
 
   return (
     <Base
@@ -109,10 +139,10 @@ export default function AddCategory() {
       className="w-8/12 mx-auto px-4 bg-cyan-600 p-8"
     >
       <div className="bg-white">
-      {goToDashboard()}
-      {successMessage()}
-      {errorMessage()}
-      {myCategoryForm()}
+        {goToDashboard()}
+        {successMessage()}
+        {errorMessage()}
+        {myCategoryForm()}
       </div>
     </Base>
   );
--- frontend/src/admin/AddProduct.js
+++ frontend/src/admin/AddProduct.js
@@ -3,7 +3,7 @@ import Base from '../core/Base'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { useEffect } from 'react'
-import { getCategories } from './helper/adminapicall'
+import { createProduct, getCategories } from './helper/adminapicall'
 import { isAuthenticated } from '../auth/helper'
 
 export default function AddProduct() {
@@ -22,7 +22,7 @@ export default function AddProduct() {
         error:"",
         createdProduct:"",
         getRedirect:false,
-        formData:""
+        formData:"",
     })
 
     const {name, description, price, stock, categories, category, loading, error, createdProduct, getRedirect, formData} = values
@@ -47,10 +47,12 @@ export default function AddProduct() {
 
     useEffect(() => {
         preload()
+        console.log("createdProduct", createdProduct)
     }, [])
 
     const handleChange = name => event => {
-        const value = name === "photo" ? event.target.file[0] : event.target.value
+      console.log("event.target.file[0]", event.target.files)
+        const value = name === "photo" ? event.target.files[0] : event.target.value
         formData.set(name, value)
         setValues({
             ...values,
@@ -60,13 +62,68 @@ export default function AddProduct() {
 
     const onSubmit = (event) => {
         event.preventDefault();
+        setValues({
+          ...values,
+          error:"",
+          loading:true
+        })
+
+        createProduct(user._id, token, formData)
+        .then(data => {
+          if(data.error){
+            setValues({...values, error:data.error})
+          }else{
+            setValues({
+              ...values,
+              name:"",
+              description:"",
+              price:"",
+              stock:"",
+              photo:"",
+              loading:false,
+              createdProduct:data.name,
+              category:""
+            })
+          }
+        })
+        .catch(err => console.error("error"))
     } 
+
+    const successMessage = () => {
+      return (
+        <div className="flex items-center justify-center w-full">
+          <div className="w-1/2 px-6">
+            <div
+              className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
+              role="alert"
+              style={{ display: createProduct ? "" : "none" }}
+            >
+              <span className="font-bold">{createProduct} created successsfully</span>
+              <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                <svg
+                  className="fill-current h-6 w-6 text-green-500"
+                  role="button"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                >
+                  <title>Close</title>
+                  <path
+                    d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+              
+                  />
+                </svg>
+              </span>
+            </div>
+          </div>
+        </div>
+      );
+    };
 
     const createProductForm = () => (
         <form className='mx-auto'>
             <div className="w-9/12 mx-auto px-12 py-4">
             <span className='text-white text-base'>Post photo</span>
-            <div className="w-full text-center bg-green-600 p-2 rounded rounded-sm text-white">
+            <div className="w-full text-center bg-green-600 p-2 rounded-sm text-white">
               <label>
                 <input
                   onChange={handleChange("photo")}
@@ -116,7 +173,7 @@ export default function AddProduct() {
             </div>
             <div className="my-2">
               <input
-                onChange={handleChange("quantity")}
+                onChange={handleChange("stock")}
                 type="number"
                 className="block w-full rounded border border-solid border-white-600  bg-clip-padding px-3 py-[0.25rem] text-base font-normal leading-[1.6] text-neutral-700 outline-none focus:border-none focus:text-neutral-700 focus:outline-none focus:ring dark:border-white-600"
                 placeholder="Quantity"
@@ -136,9 +193,36 @@ export default function AddProduct() {
         </form>
       );
     
+      const errorMessage = () => {
+        return (
+          <div className="flex items-center justify-center w-full">
+            <div className="w-1/2 px-6">
+              <div
+                className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+                role="alert"
+                style={{ display: error ? "" : "none" }}
+              >
+                <span className="font-bold">Failed to create {createProduct}</span>
+                <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
+                  <svg
+                    className="fill-current h-6 w-6 text-red-500"
+                    role="button"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <title>Close</title>
+                    <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
+                  </svg>
+                </span>
+              </div>
+            </div>
+          </div>
+        );
+      };
 
   return (
     <Base title='Add a Product here!' description='Welcome to product creation section' className='w-8/12 mx-auto rounded bg-cyan-600 p-4'>
+    {successMessage()}
     <div className='pt-3'>
         <Link to='/admin/dashboard' className="mt-2 bg-gray-600 text-sm px-4 py-2 text-white border border-transparent transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-700 duration-300">
         Admin Home
